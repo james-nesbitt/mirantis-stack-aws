abd6063c4e88c0167c370ce139e7b893b2ea2846
James Nesbitt <jnesbitt@mirantis.com>
2024-03-18 16:01:07 +0200

    launchpad msr
    - launchpad now installs msr
    	- ingress for msr
    	- firewall for msr (permissive)
    - clean up mke/msr flags across provision and yaml output
    
    ALSO
    
    - better comments in the tfvars tpl
    - more default tags for user/expiry
    
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- launchpad-stack-aws3/infra/main.tf
+++ launchpad-stack-aws3/infra/main.tf
@@ -1,13 +1,19 @@
 
 resource "time_static" "now" {}
 
+# get our (client) id
+data "aws_caller_identity" "current" {}
+
 locals {
 
   // build some tags for all things
   tags = merge(
     { # excludes kube-specific tags
-      "stack"   = var.name
-      "created" = time_static.now.rfc3339
+      "stack"      = var.name
+      "created"    = time_static.now.rfc3339
+      "expire"     = timeadd(time_static.now.rfc3339, "72h")
+      "user_id"    = data.aws_caller_identity.current.user_id
+      "account_id" = data.aws_caller_identity.current.account_id
     },
     var.extra_tags
   )
